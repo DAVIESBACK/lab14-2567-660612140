4c1b76df503c9a30c51060452ca34a13b690e497
win <wwinnn478@gmail.com>
2024-09-07 18:41:42 +0700

    done5

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,8 @@
+import { Text } from "@mantine/core";
 export default function Footer() {
   return (
     <div>
-      <p>Copyright © 2024 Chanachai Chamnanmor 660612140</p>
+      <Text c="dimmed" ta="center">Copyright © 2024 Chanachai Chamnanmor 660612140</Text>
     </div>
   );
 }
